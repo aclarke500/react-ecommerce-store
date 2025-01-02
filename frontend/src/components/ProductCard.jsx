eb57d3e8b9685b4f1384b89e7d7dfd24754b302a
Adam Clarke <aclarke500@gmail.com>
2025-01-01 23:48:33 -0500

    updated product card to fetch img and updated styling

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,11 +1,38 @@
-import { Link } from 'react-router-dom'
-import '../App.css'
+import { useState, useEffect } from 'react'
+import '../styles/ProductCard.scss';
+import { getProductImageFromId } from '../backend/product';
+
+
 export default function ProductCard(props) {
-    return <Link  to={`/product/${props.product.id}`} className="product-card">
-    <div >
-        <h2>{props.product.name}</h2>
-        <h3>Price: ${props.product.price}</h3>
-    </div>
-    </Link>
+    const [imageSrc, setImageSrc] = useState(null);
+
+    useEffect(() => {
+            async function fetchImage() {
+                try {
+                    const imageUrl = await getProductImageFromId(props.product.id);
+                    setImageSrc(imageUrl);
+                } catch (error) {
+                    console.error("Error fetching product image:", error);
+                }
+            }
+    
+            fetchImage();
+        }, [props.product.id]);
+
+
+    const handleClick = () => {
+        window.location.href = `/product/${props.product.id}`;
+    };
+
+    return (
+        <div onClick={handleClick} className="product-card">
+            <img src={imageSrc}/>
+            <div className="product-tag">
+                <p className='name'>{props.product.name}</p>
+                {/* <p className='description'>{props.product.description}</p> */}
+                <p className='price'>${props.product.price}</p>
+            </div>
+        </div>
+    );
 
 }
