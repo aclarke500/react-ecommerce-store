a82be170a43ea23f8687e03bb209df2234de6acc
Adam Clarke <aclarke500@gmail.com>
2025-01-02 16:30:57 -0500

    added params to app.run fn

--- backend/app.py
+++ backend/app.py
@@ -140,4 +140,4 @@ def get_all_products():
 # Run the Flask app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000)) # Use the PORT environment variable if it exists
-    app.run()
+    app.run(host="0.0.0.0", port=port)
