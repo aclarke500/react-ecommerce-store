e1fd0b81439fe6b1fedbb71b5be3628eb946d9f1
Adam Clarke <aclarke500@gmail.com>
2025-01-02 15:00:45 -0500

    set app ready for deployment

--- backend/app.py
+++ backend/app.py
@@ -136,4 +136,4 @@ def get_all_products():
         return jsonify({"error": str(e)}), 500
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run()
