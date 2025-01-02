c3cc66f25b605473faf8a3d2d57f292fc9910a3a
Adam Clarke <aclarke500@gmail.com>
2025-01-01 21:59:12 -0500

    added img path to vector db utils

--- backend/build_database/vector_db_utils.py
+++ backend/build_database/vector_db_utils.py
@@ -1,9 +1,15 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
+import pandas as pd
 
 # Load the SentenceTransformer model
 model = SentenceTransformer('all-MPNet-base-v2')
-
+temp_img_df = pd.read_csv('temp.csv')
+def get_img_url(id):
+    url = temp_img_df.loc[temp_img_df['product_id'] == id, 'product_img_url'].values
+    if len(url) == 0: # If the image URL is not found, return a default image
+        return "https://oaidalleapiprodscus.blob.core.windows.net/private/org-GMnbtRP4ficn3DyHiO8HT2Ou/user-uBxP2K66QnLbLi7g9cnyXmw2/img-TLEVdVMIcKMwa5xCaRAp0WCs.png?st=2025-01-02T01%3A52%3A04Z&se=2025-01-02T03%3A52%3A04Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-01-02T01%3A28%3A35Z&ske=2025-01-03T01%3A28%3A35Z&sks=b&skv=2024-08-04&sig=CIyBbjhvuKd1OcWdZ5AcTJDHFx3tWbsa%2BoWAI8of8ro%3D"
+    return url[0]
 
 def embed_item(item: dict) -> np.ndarray:
     """
@@ -37,5 +43,8 @@ def create_vector_schema(meta_data: list[dict]) -> list[dict]:
     for item in meta_data:
         vector = embed_item(item)
         item['vector'] = vector
+        img_url = get_img_url(item['id'])
+        item['img_url'] = img_url
         vector_schema.append(item)
     return vector_schema 
+
